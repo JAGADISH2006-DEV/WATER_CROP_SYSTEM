e06ec2cbf6db0d182bec77dceeea941aca0fd4dc
JAGADISH <74805995+JAGADISH2006-DEV@users.noreply.github.com>
2026-01-04 23:08:39 +0530

    Update OUTLET_NODE_V1.ino

--- OUTLET_NODE_V1.ino
+++ OUTLET_NODE_V1.ino
@@ -1,18 +1,14 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-/* ================= CONFIG ================= */
-#define RELAY_PIN D1          // ACTIVE-LOW relay
+#define RELAY_PIN D1
+#define LED_PIN   LED_BUILTIN   // ACTIVE LOW
+#define AP_SSID   "WATERSYSTEM"
+#define AP_PASS   "12345678"
 
-IPAddress localIP(192,168,4,3);
-IPAddress gateway(192,168,4,1);
-IPAddress subnet(255,255,255,0);
-
-/* ================= SERVER ================= */
 ESP8266WebServer server(80);
 bool valveOpen = false;
 
-/* ================= RELAY ================= */
 inline void valveClose() {
   digitalWrite(RELAY_PIN, HIGH);
   valveOpen = false;
@@ -23,16 +19,36 @@ inline void valveOpenFn() {
   valveOpen = true;
 }
 
-/* ================= SETUP ================= */
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);
+  pinMode(LED_PIN, OUTPUT);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_PIN, LOW); delay(200);
+    digitalWrite(LED_PIN, HIGH); delay(800);
+
+    int n = WiFi.scanNetworks();
+    for (int i = 0; i < n; i++) {
+      if (WiFi.SSID(i) == AP_SSID) {
+        WiFi.begin(AP_SSID, AP_PASS);
+        while (WiFi.status() != WL_CONNECTED) {
+          digitalWrite(LED_PIN, LOW); delay(100);
+          digitalWrite(LED_PIN, HIGH); delay(100);
+        }
+        digitalWrite(LED_PIN, LOW);
+        return;
+      }
+    }
+  }
+}
+
 void setup() {
+  Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);
   valveClose();
 
-  WiFi.mode(WIFI_STA);
-  WiFi.config(localIP, gateway, subnet);
-  WiFi.begin("WATER_SYSTEM", "12345678");
+  connectWiFi();
 
-  /* ---------- ENDPOINTS ---------- */
   server.on("/open", []() {
     valveOpenFn();
     server.send(200, "text/plain", "OK");
@@ -46,8 +62,8 @@ void setup() {
   server.begin();
 }
 
-/* ================= LOOP ================= */
 void loop() {
   server.handleClient();
-  yield();
+  if (WiFi.status() != WL_CONNECTED)
+    digitalWrite(LED_PIN, HIGH);
 }
