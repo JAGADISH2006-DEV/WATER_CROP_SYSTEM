f095d864d42ac5685d9814653b44fe3a21ae1fd6
JAGADISH <74805995+JAGADISH2006-DEV@users.noreply.github.com>
2026-01-04 23:08:15 +0530

    Update INLET_NODE_V1.ino

--- INLET_NODE_V1.ino
+++ INLET_NODE_V1.ino
@@ -1,20 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
-/* ================= CONFIG ================= */
-#define RELAY_PIN D1          // ACTIVE-LOW relay
+#define RELAY_PIN   D5
+#define LED_PIN     LED_BUILTIN   // ACTIVE LOW
 #define FAILSAFE_MS 6000
+#define AP_SSID     "WATERSYSTEM"
+#define AP_PASS     "12345678"
 
-IPAddress localIP(192,168,4,2);
-IPAddress gateway(192,168,4,1);
-IPAddress subnet(255,255,255,0);
-
-/* ================= SERVER ================= */
 ESP8266WebServer server(80);
 unsigned long lastCmdTime = 0;
 bool valveOpen = false;
 
-/* ================= RELAY ================= */
 inline void valveClose() {
   digitalWrite(RELAY_PIN, HIGH);
   valveOpen = false;
@@ -23,21 +19,41 @@ inline void valveClose() {
 inline void valveOpenFn() {
   digitalWrite(RELAY_PIN, LOW);
   valveOpen = true;
+  lastCmdTime = millis();
 }
 
-/* ================= SETUP ================= */
+void connectWiFi() {
+  WiFi.mode(WIFI_STA);
+  pinMode(LED_PIN, OUTPUT);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_PIN, LOW); delay(200);
+    digitalWrite(LED_PIN, HIGH); delay(800);
+
+    int n = WiFi.scanNetworks();
+    for (int i = 0; i < n; i++) {
+      if (WiFi.SSID(i) == AP_SSID) {
+        WiFi.begin(AP_SSID, AP_PASS);
+        while (WiFi.status() != WL_CONNECTED) {
+          digitalWrite(LED_PIN, LOW); delay(100);
+          digitalWrite(LED_PIN, HIGH); delay(100);
+        }
+        digitalWrite(LED_PIN, LOW); // SOLID ON
+        return;
+      }
+    }
+  }
+}
+
 void setup() {
+  Serial.begin(115200);
   pinMode(RELAY_PIN, OUTPUT);
   valveClose();
 
-  WiFi.mode(WIFI_STA);
-  WiFi.config(localIP, gateway, subnet);
-  WiFi.begin("WATER_SYSTEM", "12345678");
+  connectWiFi();
 
-  /* ---------- ENDPOINTS ---------- */
   server.on("/open", []() {
     valveOpenFn();
-    lastCmdTime = millis();
     server.send(200, "text/plain", "OK");
   });
 
@@ -46,17 +62,20 @@ void setup() {
     server.send(200, "text/plain", "OK");
   });
 
+  server.on("/ping", []() {
+    lastCmdTime = millis();
+    server.send(200, "text/plain", "OK");
+  });
+
   server.begin();
 }
 
-/* ================= LOOP ================= */
 void loop() {
   server.handleClient();
 
-  /* ----- FAIL SAFE ----- */
-  if (valveOpen && millis() - lastCmdTime > FAILSAFE_MS) {
-    valveClose();
-  }
+  if (WiFi.status() != WL_CONNECTED)
+    digitalWrite(LED_PIN, HIGH); // OFF
 
-  yield();
+  if (valveOpen && millis() - lastCmdTime > FAILSAFE_MS)
+    valveClose();
 }
